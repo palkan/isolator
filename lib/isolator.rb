a0fe780508511a2c3271436417a0bc24ac0f7713
Alexander Shagov <x48j35@gmail.com>
2018-02-12 17:08:37 +0300

    Add basic Isolator interface

--- lib/isolator.rb
+++ lib/isolator.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
-require "isolator/version"
+require "sniffer"
+
+Dir["#{File.dirname(__FILE__)}/isolator/**/*.rb"].each { |f| require f }
 
 module Isolator # :nodoc:
+  def self.start_analyze
+    return unless block_given?
+
+    analyzers.each(&:start)
+    yield
+    analyzers.each do |analyzer|
+      if analyzer.find_something?
+        analyzer.end
+        analyzer.raise_error!
+      end
+    end
+  end
+
+  def self.analyzers
+    [
+      Isolator::Analyzers::HTTP.new
+    ]
+  end
 end
