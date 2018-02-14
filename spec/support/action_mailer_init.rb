63c169ba86936e6c1cfc41dcd8aef5bd0f713657
Alexander Shagov <x48j35@gmail.com>
2018-02-14 21:43:38 +0300

    Add ActionMailer to test dependencies

---
+++ spec/support/action_mailer_init.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.delivery_method = :test
+
+class SampleEmail < ActionMailer::Base
+  default from: 'isolator@example.com'
+
+  def hello
+    mail(to: 'example.com', subject: 'isolator test', body: 'isolator test')
+  end
+end
