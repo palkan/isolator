52e5eaac80d3568f121d13d3a59433a024b1b08d
Alexander Shagov <x48j35@gmail.com>
2018-02-16 15:02:33 +0300

    Add more specs for ActionMailer adapter

--- lib/isolator/adapters/action_mailer.rb
+++ lib/isolator/adapters/action_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ActiveSupport.on_load(:action_mailer) do
-  Isolator.isolate :action_mailer, ActionMailer::Base,
-                   :mail, exception_class: Isolator::ActionMailerError
+  Isolator.isolate :action_mailer, ActionMailer::Base.singleton_class,
+                   :deliver_mail, exception_class: Isolator::ActionMailerError
 end
