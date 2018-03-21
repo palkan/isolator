cb12d16f9f98737df180b2e5c68656cb1d7ca658
Salahutdinov Dmitry <dsalahutdinov@gmail.com>
2018-03-21 19:43:13 +0500

    move back to method_missing

--- lib/isolator/adapters/config.rb
+++ lib/isolator/adapters/config.rb
@@ -8,18 +8,30 @@ module Isolator
     class Config
       include Enumerable
       extend Forwardable
-      def_delegators :hash_config, :each, :[]
+      def_delegators :store, :each
 
       def register(id, adapter)
-        raise "Adapter already registered: #{id}" if respond_to?(id)
-        define_singleton_method(id) { self[id] }
-        hash_config[id] = adapter
+        raise "Adapter already registered: #{id}" if store.key?(id.to_s)
+        store[id.to_s] = adapter
+      end
+
+      def [](adapter_id)
+        store.fetch(adapter_id.to_s)
+      end
+
+      def method_missing(key, *_args)
+        self[key] || super
+      end
+
+      def respond_to_missing?(key, *_args)
+        key_str = key.to_s
+        store.key?(key_str)
       end
 
       private
 
-      def hash_config
-        @hash_config ||= {}
+      def store
+        @store ||= {}
       end
     end
   end
--- spec/isolator/adapters/config_spec.rb
+++ spec/isolator/adapters/config_spec.rb
@@ -12,8 +12,21 @@ describe Isolator::Adapters::Config do
     expect(subject.sidekiq).to eq adapter
   end
 
+  it "allows to use index by adapter id" do
+    expect(subject[:sidekiq]).to eq adapter
+  end
+
+  it "raises KeyError when try to get unregistered adapter config" do
+    expect { subject.not_sidekiq }.to raise_error KeyError
+  end
+
+  it "respondes to respond_to?" do
+    expect(subject.respond_to?(:sidekiq)).to eq true
+    expect(subject.respond_to?(:not_sidekiq)).to eq false
+  end
+
   it "enumerates with each (f.e. using map)" do
     adapters_hash = Hash[subject.map { |k, v| [k, v] }]
-    expect(adapters_hash).to eq(sidekiq: adapter)
+    expect(adapters_hash).to eq("sidekiq" => adapter)
   end
 end
