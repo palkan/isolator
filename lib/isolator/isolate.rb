e9412a282046742475044ff38d3ac64ddc2cf0b1
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-03-02 18:38:24 -0500

    Fix 2.7 warnings

--- lib/isolator/isolate.rb
+++ lib/isolator/isolate.rb
@@ -5,7 +5,7 @@ module Isolator
   module Isolate
     def isolate(id, **options)
       raise "Adapter already registered: #{id}" if Isolator.adapters.key?(id.to_s)
-      adapter = AdapterBuilder.call(options)
+      adapter = AdapterBuilder.call(**options)
       Isolator.adapters[id.to_s] = adapter
     end
   end
