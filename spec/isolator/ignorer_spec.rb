269f879bae1baa619ae3849cec594b0cb69676ae
Tomasz Gieniusz <tomasz.gieniusz@certsy.com>
2023-05-12 17:25:37 +1000

    Add specs

--- spec/isolator/ignorer_spec.rb
+++ spec/isolator/ignorer_spec.rb
@@ -3,78 +3,102 @@
 require "spec_helper"
 
 describe "Ignorer" do
-  let(:todo_path) { ".isolator_todo.yml" }
+  let(:todo_temp_file) { Tempfile.new('.isolator_todo.yml') }
+  let(:todo_path) { todo_temp_file.path }
 
-  before(:all) do
+  before(:each) do
     module ::Isolator::Danger # rubocop:disable Lint/ConstantDefinitionInBlock
-      def self.call_masked(a, b)
+      def self.call_foo(a, b)
         a + b
       end
 
-      def self.call_unmasked(a, b)
+      def self.call_bar(a, b)
         a + b
       end
     end
 
-    Isolator.isolate :todo_masked_adapter,
+    Isolator.isolate :todo_foo_adapter,
       target: ::Isolator::Danger.singleton_class,
-      method_name: :call_masked
+      method_name: :call_foo
 
-    Isolator.isolate :todo_unmasked_adapter,
+    Isolator.isolate :todo_bar_adapter,
       target: ::Isolator::Danger.singleton_class,
-      method_name: :call_unmasked
+      method_name: :call_bar
   end
 
-  after(:all) do
+  after(:each) do
     Isolator.send(:remove_const, "Danger")
-    Isolator.adapters.delete("todo_masked_adapter")
-    Isolator.adapters.delete("todo_unmasked_adapter")
+    Isolator.adapters.delete("todo_foo_adapter")
+    Isolator.adapters.delete("todo_bar_adapter")
   end
 
   before do
     allow(Isolator).to receive(:within_transaction?) { true }
-    allow(File).to receive(:exist?).and_call_original
-    allow(File).to receive(:exist?).with(todo_path).and_return(true)
   end
 
   subject { ::Isolator::Danger }
 
   specify do
-    expect { subject.call_masked(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
-    expect { subject.call_unmasked(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
+    expect { subject.call_foo(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
+    expect { subject.call_bar(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
   end
 
   shared_examples "todos filter" do
-    context "unmasked todos" do
+    context "exact files paths todos" do
       before do
-        allow(YAML).to receive(:load_file).with(todo_path).and_return(
-          "todo_unmasked_adapter" => ["spec/isolator/ignorer_spec.rb:59"],
-          "wrong_adapter" => ["spec/isolator/ignorer_spec.rb:62"]
-        )
+        todo_temp_file.write(%{
+          todo_foo_adapter:
+            - spec/isolator/ignorer_spec.rb:61
+          wrong_adapter:
+            - spec/isolator/ignorer_spec.rb:65
+        })
+        todo_temp_file.close
 
         prepare
       end
 
       it "doesn't raise when ignored" do
-        expect { subject.call_unmasked(1, 2) }.not_to raise_error
+        expect { subject.call_foo(1, 2) }.not_to raise_error
       end
 
       it "raise when wrong operator is ignored" do
-        expect { subject.call_unmasked(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
+        expect { subject.call_bar(1, 2) }.to raise_error(Isolator::UnsafeOperationError)
       end
     end
 
-    context "masked todos" do
+    context "wildcard glob pattern todos" do
       before do
-        allow(YAML).to receive(:load_file).with(todo_path).and_return(
-          "todo_masked_adapter" => ["spec/isolator/**/*.rb"]
-        )
+        todo_temp_file.write(%{
+          todo_foo_adapter:
+            - spec/isolator/**/*.rb
+        })
+        todo_temp_file.close
 
         prepare
       end
 
-      it "doesn't raise when ignored via mask" do
-        expect { subject.call_masked(1, 2) }.not_to raise_error
+      it "doesn't raise when ignored" do
+        expect { subject.call_foo(1, 2) }.not_to raise_error
+      end
+    end
+
+    context "common yml alias reused in multiple adapters in todos" do
+      before do
+        todo_temp_file.write(%{
+          common: &common
+            - spec/isolator/**/*.rb
+
+          todo_foo_adapter: *common
+          todo_bar_adapter: *common
+        })
+        todo_temp_file.close
+
+        prepare
+      end
+
+      it "doesn't raise when ignored", :aggregate_failures do
+        expect { subject.call_foo(1, 2) }.not_to raise_error
+        expect { subject.call_bar(1, 2) }.not_to raise_error
       end
     end
   end
@@ -85,7 +109,7 @@ describe "Ignorer" do
 
   context "when the file is not parsed to a hash" do
     before do
-      allow(YAML).to receive(:load_file).with(todo_path).and_return(nil)
+      todo_temp_file.close
     end
 
     it "raises an error" do
