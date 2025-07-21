f17bb00e18dbf2e78cd08695efc73a1e6aaa5277
Sunny Ripert <sunny@sunfox.org>
2025-07-21 14:19:07 +0200

    Fix Rails 8.0 support

--- lib/isolator/orm_adapters/active_record.rb
+++ lib/isolator/orm_adapters/active_record.rb
@@ -3,7 +3,7 @@
 require_relative "active_support_subscriber"
 
 # We rely on this feature introduced in 7.1.0.beta1: https://github.com/rails/rails/pull/49192
-if ActiveRecord::VERSION::MAJOR >= 7 && ActiveRecord::VERSION::MINOR >= 1
+if ActiveRecord.version >= "7.1"
   require_relative "active_support_transaction_subscriber"
   Isolator::ActiveSupportTransactionSubscriber.subscribe!
 else
